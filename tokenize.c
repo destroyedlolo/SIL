920e0f760f4f297427216c161a23b3ad5f77b9c9
destroyedlolo <l_faillie@yahoo.com>
2021-01-13 02:12:22 +0100

    Parse comments

--- tokenize.c
+++ tokenize.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include <errno.h>
 
 struct reader {
@@ -35,14 +36,67 @@ void init_reader(struct reader *r, struct storage *s){
 
 
 /*****
+ * Parser
+ *****/
+
+bool parse( struct reader *reader ){
+	char l[MAXLINE];
+	enum resReading r;
+	int nested_comment = 0;	/* level of nested comments */
+
+	while( !(r=reader->readline(reader, l, MAXLINE)) ){
+		char *s = l;
+
+		while( isspace(*s) ) s++;
+
+				/* it's safe to check next character as in worst case,
+				 * at the end of the buffer, the last one is provisioned for \0
+				 */
+		if(!*s)
+			continue;
+		if(*s == '#'){
+			if(s[1] == '*')	/* Multi line comment */
+				nested_comment++;
+			continue;	/* remaining of the line can be ignored */
+		}
+		if(nested_comment && *s == '*'){
+			if(s[1] == '#'){
+				nested_comment--;
+				continue;	/*TODO : check if the remaining is empty*/
+			}
+			/* otherwise, let's continue with the parsing */
+		}
+
+		if(nested_comment)	/* inside comments */
+			continue;
+
+		puts(l);
+	}
+
+	if(nested_comment){
+		errstr = "Unbalanced comment";
+		return false;
+	}
+
+	return( r==FINISHED );
+}
+
+
+/*****
  * Reading from a file
  ****/
 
 enum resReading getLineFromFile( struct reader *reader, char *line, size_t max ){
+	size_t l;
+
 	if(!fgets(line, max, reader->file))
-		return ERROR;
-	if(ferror(reader->file))
-		return FINISHED;
+		return(feof(reader->file) ? FINISHED : ERROR);
+
+		/* remove trailing \n */
+	l = strlen(line);
+	if(l && line[--l] == '\n')
+		line[l] = 0;
+
 	return OK;
 }
 
@@ -56,5 +110,6 @@ bool tokenizeFile( struct storage *storage, const char *file ){
 		return false;
 	}
 
-	return true;
+	return parse(&rdr);
 }
+
