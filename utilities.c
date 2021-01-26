23352a7169e99eff0569790f2e036257ed5a7ff6
destroyedlolo <l_faillie@yahoo.com>
2021-01-26 18:49:19 +0100

    Improve hash calculation

--- utilities.c
+++ utilities.c
@@ -7,10 +7,10 @@
 hash_t hash( const char *s, size_t l){
 	hash_t h=0;
 	
-	for(size_t i=l; i; i--){
+	for(size_t i=l,j=0; i; i--,j++){
 		if(!*s)	/* end of the string */
 			break;
-		h += *s++;
+		h += ((hash_t)*s++)<<(j%8);
 	}
 
 	return h;
